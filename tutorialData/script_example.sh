05ca558c3b3cfe80a277f3e680b16d72d0969202
heinc1010 <40910863+heinc1010@users.noreply.github.com>
2018-10-27 05:54:28 +0000

    Create script_example.sh

---
+++ tutorialData/script_example.sh
@@ -0,0 +1,6 @@
+#command to run the tutorial
+python /path/to/MuBaMer/MuBaMer.py \
+-d /path/to/MuBaMer/tutorialData/ \
+-o /output/dir/ \
+-r /hg38/reference/genome.fa \
+-b /path/to/MuBaMer/tutorialData/targeted.bed
