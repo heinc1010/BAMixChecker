31ddb8a1b218feb2b38f7b9aaee16b4e68c36970
heinc1010 <40910863+heinc1010@users.noreply.github.com>
2018-10-27 05:55:07 +0000

    Update script_example.sh

--- tutorialData/script_example.sh
+++ tutorialData/script_example.sh
@@ -3,4 +3,4 @@ python /path/to/MuBaMer/MuBaMer.py \
 -d /path/to/MuBaMer/tutorialData/ \
 -o /output/dir/ \
 -r /hg38/reference/genome.fa \
--b /path/to/MuBaMer/tutorialData/targeted.bed
+-b /path/to/MuBaMer/tutorialData/target.bed
