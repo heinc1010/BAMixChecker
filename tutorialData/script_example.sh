c95c205f1f76e502c34d31f71fd1a7f2481ff34c
heinc1010 <40910863+heinc1010@users.noreply.github.com>
2019-01-21 05:07:17 +0000

    Update script_example.sh

--- tutorialData/script_example.sh
+++ tutorialData/script_example.sh
@@ -11,6 +11,11 @@ REF=/path/of/reference/hg38/genome.fa
 
 #Command to run the tutorial data 
 #Normal case
+echo "python $BAMixChecker_PATH/BAMixChecker.py \
+-d $BAMixChecker_PATH/tutorialData/normal \
+-o $OUT_DIR/normal \
+-r $REF \
+-b $BAMixChecker_PATH/tutorialData/target.bed"
 python $BAMixChecker_PATH/BAMixChecker.py \
 -d $BAMixChecker_PATH/tutorialData/normal \
 -o $OUT_DIR/normal \
@@ -19,6 +24,11 @@ python $BAMixChecker_PATH/BAMixChecker.py \
 
 #Mismatched sample exist case
 #'S1254_N.bam' and 'S1345_T.bam ' are swapped, 'S1983_N.bam' and 'S1983_T.bam are unpaired.
+echo "python $BAMixChecker_PATH/BAMixChecker.py \
+-d $BAMixChecker_PATH/tutorialData/mismatched \
+-o $OUT_DIR/mismatched \
+-r $REF \
+-b $BAMixChecker_PATH/tutorialData/target.bed"
 python $BAMixChecker_PATH/BAMixChecker.py \
 -d $BAMixChecker_PATH/tutorialData/mismatched \
 -o $OUT_DIR/mismatched \
